eec82d8030c071f263070d09f069e165cd70a112
ChaosAttractor <lostattractor@gmail.com>
2024-07-25 05:37:54 +0800

    init commit

---
+++ configuration/default.nix
@@ -0,0 +1,23 @@
+{ pkgs, ... }:
+
+{
+  imports = [
+    ./features/nix.nix
+    ./features/fish.nix
+    ./features/avahi.nix
+    ./k3s
+  ];
+
+  users = {
+    # Don't allow mutation of users outside of the config.
+    mutableUsers = false;
+    # Privilege User
+    users.root.openssh.authorizedKeys.keys = [ "ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBC5HypvbsI4xvwfd4Uw7D+SV0AevYPS/nCarFwfBwrMHKybbqUJV1cLM1ySZPxXcZD7+3m48Riiwlssh6o7WM/M= openpgp:0xDE4C24F6" ];
+  };
+
+  networking.nftables.enable = true;
+
+  sops.defaultSopsFile = ../secrets.yaml;
+
+  system.stateVersion = "24.05";
+}
